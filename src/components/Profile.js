25ddee694ccbf6e864fa755e5d22cb2da6d9c92e
Francis Enemuo <francenemuo@gmail.com>
2022-11-01 19:28:35 +0100

    fix: change the username to twitter username

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,7 +6,7 @@ export default function Profile() {
   return (
     <div>
         <img src={pic} alt="Franc smiling" className='img-avatar' id="profile_img"/>
-        <h1 className='title' id="twitter">Franc Enemuo</h1>
+        <h1 className='title' id="twitter">@coderite_</h1>
         <h3 id="slack">Franc</h3>
     </div>
   )
