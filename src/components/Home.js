b3fe1d3190c28fefe5f4a0f740fa2a9a226802bf
Francis Enemuo <francenemuo@gmail.com>
2022-11-05 08:43:09 +0100

    fix: create route for my app

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile from './Profile'
+import Parts from './Parts'
+
+function Home() {
+  return (
+    <div className='container'>
+        <Profile />
+        <Parts />
+    </div>
+  )
+}
+
+export default Home
