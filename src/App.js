1f34d48dae212a8db59b41d54c7db6a4f6636598
Francis Enemuo <francenemuo@gmail.com>
2022-11-05 08:47:43 +0100

    fix: the container element was given a class name container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Contact from './components/Contact';
 import Home from './components/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
-      <Home />
+   
       </div>
-      <Route path='/' component={Home}/>
-      <Route path='/contact' component={Contact}/>
-    </BrowserRouter>
+      <Switch>
+      <Route path='/' exact>
+        <Home />
+      </Route>
+      <Route path='/contact' >
+        <Contact />
+      </Route>
+      </Switch>
+      
+      
+    </Router>
   );
 }
 
