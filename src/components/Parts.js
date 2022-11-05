3b02a2e6b6834d0d546b96484c55bf92e778248f
Francis Enemuo <francenemuo@gmail.com>
2022-11-05 18:29:02 +0100

    fix: remove an import not used

--- src/components/Parts.js
+++ src/components/Parts.js
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Contact from './Contact'
 
 export default function Parts() {
   return (
