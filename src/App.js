9fb33665d796189db46c396e834209343ce2be02
Francis Enemuo <francenemuo@gmail.com>
2022-11-05 06:06:00 +0100

    feat: install react router to implement links on anchor elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
+import {BrowserRouter} from 'react-router-dom';
 import Profile from './components/Profile';
 import './App.css';
 import Link from './components/Link';
 
 function App() {
   return (
-    <div className="App">
-     <Profile />
-     <Link />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <Profile />
+      <Link />
+      </div>
+    </BrowserRouter>
   );
 }
 
