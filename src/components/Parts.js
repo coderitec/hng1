b6e8d0af14d6e95d6bbdf6d197ef0ea22ebeb18e
Francis Enemuo <francenemuo@gmail.com>
2022-11-05 08:43:54 +0100

    fix: rename link.js to parts

---
+++ src/components/Parts.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Parts() {
+  return (
+    <div className='link-section'>
+       
+            <a href = "https://twitter.com/Coderite_">Twitter Link</a>
+        
+            <a href = "https://training.zuri.team/" id='btn-zuri'>Zuri Team</a>
+      
+            <a href = "http://books.zuri.team" id='books'>Zuri Books</a>
+    
+            <a href = "https://books.zuri.team/python-for-beginners?ref_id=franc" id='book__python'>Python Books</a>
+        
+            <a href = "https://background.zuri.team" id='pitch'>Connect with me</a>
+    
+            <a href = "https://books.zuri.team/design-rules" id='book_design'>Design Books</a> 
+
+            <Link to = "#" id='contact'>Contact</Link>       
+    </div>
+  )
+}
